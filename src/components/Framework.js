c1764a4a5c5b8a57303ee6e688973e6c7e1b8940
macborkow <macborkow@gmail.com>
2021-03-12 13:27:49 +0100

    Hello cubes

--- src/components/Framework.js
+++ src/components/Framework.js
@@ -1,39 +1,13 @@
 import { useEffect } from "react";
-import * as THREE from "three";
+import run from "../framework/main";
 
 const Framework = () => {
   useEffect(() => {
     const canvas = document.querySelector("#c");
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-    camera.position.z = 2;
-    const scene = new THREE.Scene();
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    {
-      const light = new THREE.DirectionalLight(0xffffff, 1);
-      light.position.set(-1, 2, 4);
-      scene.add(light);
-    }
-
-    function render(time) {
-      time *= 0.001;
-
-      cube.rotation.x = time;
-      cube.rotation.y = time;
-
-      renderer.render(scene, camera);
-
-      requestAnimationFrame(render);
-    }
-
-    requestAnimationFrame(render);
+    run(canvas);
   });
 
-  return <canvas id="c" width="640" height="320"></canvas>;
+  return <canvas id="c"></canvas>;
 };
 
 export default Framework;
---
+++ src/framework/main.js
@@ -0,0 +1,66 @@
+import * as THREE from "three";
+
+const resizeRendererToDisplaySize = (renderer) => {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+};
+
+const makeInstance = (geometry, color, x, scene) => {
+  const material = new THREE.MeshPhongMaterial({ color });
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  cube.position.x = x;
+
+  return cube;
+};
+
+const run = (canvas) => {
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+  camera.position.z = 2;
+  const scene = new THREE.Scene();
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const cubes = [
+    makeInstance(geometry, 0x44aa88, 0, scene),
+    makeInstance(geometry, 0x8844aa, -2, scene),
+    makeInstance(geometry, 0xaa8844, 2, scene),
+  ];
+
+  {
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
+  function render(time) {
+    time *= 0.001;
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+};
+
+export default run;
